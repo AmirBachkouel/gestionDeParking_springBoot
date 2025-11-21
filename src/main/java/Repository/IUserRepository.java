abc866bec3b518e2453591bb908d8a808612daf2
AmirBachkouel <amir.bechkouel@sesame.com.tn>
2025-11-21 23:37:34 +0100

    Creation du couche répertoire

---
+++ src/main/java/Repository/IUserRepository.java
@@ -0,0 +1,9 @@
+package Repository;
+
+import Entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends CrudRepository<User,Integer> {
+}
