b47bc11d422de601af435ef498e40dda55b9cd11
AmirBachkouel <amir.bechkouel@sesame.com.tn>
2025-11-22 22:48:59 +0100

    Creation de controlleur des utilisateurs

---
+++ src/main/java/Controller/UserContoller.java
@@ -0,0 +1,36 @@
+package Controller;
+
+import Entity.User;
+import Service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users/")
+public class UserContoller {
+    @Autowired
+    UserServiceImpl userService;
+
+    @GetMapping("GetUser/{id}")
+    public User getUser(@PathVariable int  id) {
+        return userService.getUser(id);
+    }
+    @GetMapping("GetUser/{criteria}")
+    public User getUser(@PathVariable String criteria) {
+        return userService.getUser(criteria);
+    }
+
+    @PostMapping("AddUser/")
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @DeleteMapping("DeleteUser/{id}")
+    public  void deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+    }
+    @DeleteMapping("DeleteUser/{criteria}")
+    public  void deleteUser(@PathVariable String  criteria) {
+        userService.deleteUser(criteria);
+    }
+}
