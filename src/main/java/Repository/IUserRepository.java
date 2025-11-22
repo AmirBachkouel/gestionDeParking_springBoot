a8ca2d82aabb9c4d09bad55e9af8a9d436176187
AmirBachkouel <amir.bechkouel@sesame.com.tn>
2025-11-22 18:19:33 +0100

    Modification des interfaces utilisateurs(répertoire et service

--- src/main/java/Repository/IUserRepository.java
+++ src/main/java/Repository/IUserRepository.java
@@ -3,7 +3,9 @@ package Repository;
 import Entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+git 
 @Repository
 public interface IUserRepository extends CrudRepository<User,Integer> {
+    User findUserByEmail(String email);
+    User findUserByUsername(String username);
 }
--- src/main/java/Service/IUserService.java
+++ src/main/java/Service/IUserService.java
@@ -5,10 +5,9 @@ import Entity.User;
 import java.util.List;
 
 public interface IUserService {
-    public List<User> getUsers();
     public User getUser(int id);
     public User getUser(String criteria);
-    public void addUser(User user);
+    public User addUser(User user);
     public void deleteUser(int id);
     public void deleteUser(String criteria);
 }
