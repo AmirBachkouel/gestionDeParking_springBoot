7de1f4d9082ba1eeb89683fe699576b3d3833902
AmirBachkouel <amir.bechkouel@sesame.com.tn>
2026-01-06 17:32:28 +0100

    Configuration de l'authentification

---
+++ src/main/java/com/example/demo/Security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.Security;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(crsf -> crsf.disable())
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/signup", "api/login").permitAll()
+                    //.requestMatchers("/api/resrvations/**").hasRole("USER")
+                    //.requestMatchers("/api/parkingSpots/**").hasRole("USER")
+                    //.requestMatchers("/api/parkingLots/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+                ).sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                )
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new RoleConverter());
+        return converter;
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        String secret = "k7Vh8s9J3Pq1F6x2Uo5mN8yR0vQ4zL1tXc7P9jE3rH2sW5yZ0bK6fD1nG4aQ8tV2";
+        return new NimbusJwtEncoder(new ImmutableSecret<>(secret.getBytes(StandardCharsets.UTF_8)));
+    }
+}
